dfc0403d7cf705978d11241b95322090978cbbb6
dalandaniell3 <31803681-dalandaniell3@users.noreply.replit.com>
2024-02-03 02:44:30 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,9 @@
+using System;
+
+class Program {
+  public static void Main (string[] args) {
+    string firstname = "Jericka";
+    string lastname = "Sudla";
+    Console.WriteLine ("I am " + (firstname) + (lastname) );
+  }
+}
